4b0c2c01d9d4d9d1f22c30db4d764d26794eebbf
matthewtsegay <mathewos0321@gmail.com>
2025-05-31 20:27:38 +0100

    i commit fix path problem

--- src/views/Blogpost/PostPage.vue
+++ src/views/Blogpost/PostPage.vue
@@ -38,7 +38,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-import { getPostById } from '../../services/apiServices.js'
+import { getPostById } from '../../api/postsApi.js'
 import LikeDislikeButton from '../../components/shared/LikeDislikeButtons.vue'
 
 const route = useRoute()
